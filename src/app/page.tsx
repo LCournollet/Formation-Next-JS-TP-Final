49c1d4e923894193afb5138c1be49e682d31950a
Louis <l.cournollet@gmail.Com>
2026-03-24 14:01:41 +0100

    fix image homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { SliceZone } from "@prismicio/react";
 import { createClient } from "@/prismicio";
 import { components } from "@/slices";
@@ -6,5 +7,18 @@ export default async function Homepage() {
   const client = createClient();
   const homepage = await client.getSingle("homepage");
 
-  return <SliceZone slices={homepage.data.slices} components={components} />;
+  return (
+    <>
+      <div className="relative w-full h-64">
+        <Image
+          src="/hero.jpg"
+          alt="Bannière"
+          fill
+          className="object-cover"
+          priority
+        />
+      </div>
+      <SliceZone slices={homepage.data.slices} components={components} />
+    </>
+  );
 }
